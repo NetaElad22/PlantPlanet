6b2400dc000026709bdab1c38a75340a0404e125
rom kogan <romkogan464@gmail.com>
2021-09-09 14:14:47 +0300

    before pull

---
+++ PlantPlanet/Migrations/20210830110301_pull.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PlantPlanet.Migrations
+{
+    public partial class pull : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
